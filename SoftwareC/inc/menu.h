1ca5cedcecda0aa587aa8fdeb163e9b5ac4940a1
Lautaro Merlino <lautimerlino07@gmail.com>
2026-05-18 23:20:50 -0300

    ProjectStart-Dev1

---
+++ SoftwareC/inc/menu.h
@@ -0,0 +1,13 @@
+* @file main.c
+* @author ypur name (you@domain.com)
+* @brief
+* @version 0.1
+@date 2026-05-05
+
+*
+
+@copyright Copyright (c) 2026
+#ifndef MENU_H
+#define MENU_H
+
+#endif // MENU_H
---
+++ SoftwareC/src/menu.c
@@ -0,0 +1,10 @@
+* @file main.c
+* @author ypur name (you@domain.com)
+* @brief
+* @version 0.1
+@date 2026-05-05
+
+*
+
+@copyright Copyright (c) 2026
+#include menu.h
