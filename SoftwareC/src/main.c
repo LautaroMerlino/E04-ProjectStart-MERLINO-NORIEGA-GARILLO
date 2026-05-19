55df6eb3563b9e66b4b56c14b7f696ec1da53de4
Lautaro Merlino <lautimerlino07@gmail.com>
2026-05-18 23:55:12 -0300

    ProjectStart-Dev-1

--- SoftwareC/src/main.c
+++ SoftwareC/src/main.c
@@ -1,10 +1,10 @@
-* @file main.c
-* @author ypur name (you@domain.com)
-* @brief
-* @version 0.1
-@date 2026-05-05
+/* @file main.c
+ * @author Dev-1 (lautimerlino07@gmail.com)
+ * @brief Programa principal con superloop para lectura de teclado
+ * @version 0.1
+ * @date 2026-05-18
+ */
 
-*
 
 @copyright Copyright (c) 2026
 #include <stdio.h>
@@ -14,6 +14,30 @@
 
 int main() 
 {
+    char letter = ' ';
+    //Seccion de inicializacion
+    menuUpdate();
+
+    //Loop infinito
+    while (1)
+    {
+        //Leer entrada de teclado
+        letter = getchar();
+
+        //Si se presiona escape
+        if (letter == 27) 
+        {
+            //Salir del programa
+            break;
+        }
+        
+
+
+        //Si se presiona otra tecla
+        //pasar el carácter correspondiente como parámetro a la función void menuUpdate(char);
+        menuUpdate(letter);
+    }
+
 
     return 0;
 }
--- SoftwareC/src/menu.c
+++ SoftwareC/src/menu.c
@@ -1,10 +1,9 @@
-* @file main.c
-* @author ypur name (you@domain.com)
-* @brief
-* @version 0.1
-@date 2026-05-05
-
-*
+/* @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-05-05
+ */
 
 @copyright Copyright (c) 2026
 #include menu.h
