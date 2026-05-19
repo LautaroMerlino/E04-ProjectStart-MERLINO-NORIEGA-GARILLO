2a467ea8ab198e77615217d36c86342f558ef302
Mai <agustin2nori@gmail.com>
2026-05-19 15:44:43 -0300

    dev2 0.1

---
+++ src/menu.c
@@ -0,0 +1,19 @@
+/**
+ * @file menu.c
+ * @author Noriega
+ * 
+ */
+
+#include <stdio.h>
+#include "../inc/menu.h"
+
+
+void menuInit(void){
+    //Seccion de configuración
+printf("esra es la seccion de configuración");
+}
+
+void menuUpdate(char letter){
+    //la letra presionada fue...
+printf("la letra presionada fue")
+}
